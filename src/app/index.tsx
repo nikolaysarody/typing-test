9bdf3fbc202212bae22e5491a2ca5cae970fca77
Nikolay <sarody@ya.ru>
2023-05-16 15:07:59 +0800

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TypingTestPage from '../pages/TypingTestPage';
+
+function App() {
+    return <TypingTestPage />;
+}
+
+export default App;
---
+++ src/pages/TypingTestPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TypingTestPage() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default TypingTestPage;
